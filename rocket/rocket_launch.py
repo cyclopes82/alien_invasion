a12b002c3338c50196384d6962c9ebd604613f79
Pratik Patil <cyclopes82@gmail.com>
2024-01-04 19:25:44 +0530

    Chapter 12 complet

--- rocket/rocket_launch.py
+++ rocket/rocket_launch.py
@@ -2,13 +2,12 @@ import sys
 
 import pygame
 
-from settings import Settings
-from ship import Ship
-# from bird import Bird
-from bullet import Bullet
+from rocket_settings import Settings
+from rocket import Rocket
 
 
-class AlienInvasion:
+
+class RocketLauncher:
     """Overall class to manage game assets and behavior"""
     
     def __init__(self) -> None:
@@ -22,10 +21,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Rocket Launcher")
         
-        self.ship = Ship(self)
-        self.bullets = pygame.sprite.Group()
+        self.rocket = Rocket(self)
         
         
           
@@ -34,8 +32,7 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()                    
+            self.rocket.update()
             self._update_screen()
             self.clock.tick(60)
             
@@ -55,45 +52,32 @@ class AlienInvasion:
     def _check_keydown_event(self, event):
         """Respond to keypresses"""
         if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
+            self.rocket.moving_right = True
         elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
+            self.rocket.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = True
         elif event.key == pygame.K_q:
             sys.exit()
-        elif event.key == pygame.K_SPACE:
-            self._fire_bullet()
             
     def _check_keyup_event(self, event):
         """Respond to key releases"""
         if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
+            self.rocket.moving_right = False
         elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
-            
-    def _fire_bullet(self):
-        """Create a new bullet and add it to the bullets group."""
-        if len(self.bullets) < self.settings.bullets_allowed:
-            new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)
-            
-    def _update_bullets(self):
-        """Update position of bullets and get rid of old bullets"""
-        self.bullets.update()    
-        #Get rid of bullets that have disappeared
-        for bullet in self.bullets.copy():
-            if bullet.rect.bottom <= 0:
-                self.bullets.remove(bullet)
-                #print(len(self.bullets))
-
-    
+            self.rocket.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = False
             
                                         
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
-        for bullet in self.bullets.sprites():
-            bullet.draw_bullet()
-        self.ship.blitme()
+        self.rocket.blitme()
         
         pygame.display.flip()
  
@@ -102,7 +86,7 @@ class AlienInvasion:
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
-    ai = AlienInvasion()
-    ai.run_game()
+    rl = RocketLauncher()
+    rl.run_game()
     
 
--- rocket/rocket_settings.py
+++ rocket/rocket_settings.py
@@ -7,11 +7,4 @@ class Settings:
         self.screen_height = 800
         # self.bg_color = (0, 0, 0)
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 1.5
-        
-        #Bullet Settings
-        self.bullet_speed = 2.0
-        self.bullet_width = 2
-        self.bullet_height = 16
-        self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.rocket_speed = 3.5
